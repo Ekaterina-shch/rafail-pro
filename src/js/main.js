4d0f4b96f3fc259d4c0e131fd3facb331b32710f
Ekaterina <shch.e.v@yandex.ru>
2026-03-03 13:33:52 +0700

    Создала футер. Добавила скрипт актуального года в футер. Написала стили. Поправила медизапрос. вместо 1920 поставила 1400

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,10 @@ import { initPhoneMasks } from './module/phone-mask';
 import { initStickyHeader } from './module/header-fixed';
 
 document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.js-year').forEach((el) => {
+    el.textContent = new Date().getFullYear();
+  });
+
   initStickyHeader();
   initPhoneMasks();
 });
